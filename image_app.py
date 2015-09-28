89c9c9b318b1598530892f315b7f5f9c69e3021d
Benjamin Irving <stuff@birving.com>
2015-09-28 22:11:24 +0100

    improving documentation

--- image_app.py
+++ image_app.py
@@ -1,4 +1,18 @@
 
+"""
+
+A very simple example pyside app that demonstrates dragging and dropping
+of files onto a GUI. 
+
+- This app allows dragging and dropping of an image file 
+that this then displayed in the GUI
+- Alternatively an image can be loaded using the button
+- This app includes a workaround for using pyside for dragging and dropping
+with OSx
+- This app should work on Linux, Windows and OSx
+
+"""
+
 from __future__ import division, unicode_literals, print_function, absolute_import
 
 from PySide import QtGui, QtCore
@@ -12,19 +26,28 @@ if op_sys == 'Darwin':
 
 
 class MainWindowWidget(QtGui.QWidget):
+    """
+    Subclass the widget and add a button to load images. 
+    
+    Alternatively set up dragging and dropping of image files onto the widget
+    """
 
     def __init__(self):
         super(MainWindowWidget, self).__init__()
 
+        # Button that allows loading of images
         self.load_button = QtGui.QPushButton("Load image")
         self.load_button.clicked.connect(self.load_image_but)
 
+        # Image viewing region
         self.lbl = QtGui.QLabel(self)
 
+        # A horizontal layout to include the button on the left
         layout_button = QtGui.QHBoxLayout()
         layout_button.addWidget(self.load_button)
         layout_button.addStretch()
 
+        # A Vertical layout to include the button layout and then the image
         layout = QtGui.QVBoxLayout()
         layout.addLayout(layout_button)
         layout.addWidget(self.lbl)
@@ -42,7 +65,10 @@ class MainWindowWidget(QtGui.QWidget):
 
         :return:
         """
+        
+        #Get the file location
         self.fname, _ = QtGui.QFileDialog.getOpenFileName(self, 'Open file')
+        # Load the image from the location
         self.load_image()
 
     def load_image(self):
@@ -55,6 +81,7 @@ class MainWindowWidget(QtGui.QWidget):
         pixmap = pixmap.scaled(500, 500, QtCore.Qt.KeepAspectRatio)
         self.lbl.setPixmap(pixmap)
 
+    # The following three methods set up dragging and dropping for the app
     def dragEnterEvent(self, e):
         if e.mimeData().hasUrls:
             e.accept()
@@ -90,10 +117,10 @@ class MainWindowWidget(QtGui.QWidget):
         else:
             e.ignore()
 
-
+# Run if called directly
 if __name__ == '__main__':
-    # Initialise the PKView application
+    # Initialise the application
     app = QtGui.QApplication(sys.argv)
-    # Pass arguments from the terminal (if any) into the main application
+    # Call the widget
     ex = MainWindowWidget()
     sys.exit(app.exec_())
